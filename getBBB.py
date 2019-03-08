f9bf7dba35dd1ad6215b820b506b234056b9008f
Benjamin Breshears <43872449+BenTheCyberOne@users.noreply.github.com>
2019-03-08 15:46:18 -0600

    Update getBBB.py
    Pre Compile #3 Fix #1 - Fixed obvious issue with Python syntax being wrong (input does not need a print statement inside, I was thinking about Java lol)

--- getBBB.py
+++ getBBB.py
@@ -8,15 +8,17 @@ from lxml import html
 import requests
 def main():
     print("### WARNING: CURRENTLY CAN ONLY BE USED WITH THE FOLLOWING OPTIONS:\n>A LOCATION\n>ACCREDITED FILER\n>JOB TYPE (QUERY)")
-    fileName = input(print("Please enter filename for output files(leave out the ext): "))
+    fileName = input("Please enter filename for output files(leave out the ext): ")
     f = open(fileName,"x")
-    loc = input(print("Please enter a city: (Ex. Des Moines)"))
-    loc2 = input(print("Please enter the state: (Ex. IA)"))
-    acc = input(print("Search for accredited businesses only? (y or n) "))
-    entity = input(print("Enter the job title to scrape: "))
+    loc = input("Please enter a city: (Ex. Des Moines)")
+    loc2 = input("Please enter the state: (Ex. IA)")
+    acc = input("Search for accredited businesses only? (y or n) ")
+    entity = input("Enter the job title to scrape: ")
     if acc == 'n':
         filter = ""
     else:
         filter ="filter_accredited=1&"
-    finalLink = ("https://www.bbb.org/search?"+filter+"find_country=USA&find_loc="+loc+"%2C%20"+loc2"&find_text="+entity+"&find_type=Category&page=1&sort=Relevance/")
+    finalLink = ("https://www.bbb.org/search?"+filter+"find_country=USA&find_loc="+loc+"%2C%20"+loc2+"&find_text="+entity+"&find_type=Category&page=1&sort=Relevance/")
     page = requests.get()
+if __name__ == "__main__":
+    main()
